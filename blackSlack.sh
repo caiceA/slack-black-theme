214cef78c2c643e65571460be5ac118bbed68f55
Jonathan Levin <jmlevin78@gmail.com>
2019-03-21 14:30:14 -0400

    modifications for slack posts

--- blackSlack.sh
+++ blackSlack.sh
@@ -8,6 +8,10 @@ sed '/.c-dialog__footer .c-button/i \
 .c-pillow_file__email__meta td{color: white !important} \
 .c-pillow_file__email__sender__address{color: #6ed2f2 !important} \
 .c-pillow_file__email--collapsed{color: white !important} \
+.c-pillow_file__post__content ul.checklist{background: #222222 !important} \
+.c-pillow_file__post__content ul.checklist li:before{background: #363636 !important} \
+.c-pillow_file__post__content ul.checklist li.checked{font-style: italic;} \
+.c-pillow_file__post{border-top: 1px solid #222222;} \
 \
 ' tmp_ssb-interop.js >> ssb-interop
 mv ssb-interop tmp_ssb-interop.js
