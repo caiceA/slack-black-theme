5f24392dfb74577c2a7c24412cd1c0c4d05c26e7
Tom Burgin <3825172+tgburgin@users.noreply.github.com>
2019-09-06 08:26:27 +0100

    Missing backtick

--- slack-dark-mode.ps1
+++ slack-dark-mode.ps1
@@ -36,7 +36,7 @@ try {
 
 Write-Warning "Installing Necessary Fonts for Theme, continuing install."
 
-if ((-not (Test-Path "C:\Windows\Fonts\Muli.ttf"))
+if ((-not (Test-Path "C:\Windows\Fonts\Muli.ttf")) `
     -and (-not (Test-Path -path $env:LOCALAPPDATA\Microsoft\Windows\Fonts\Muli.ttf))){
     $SourceDir   = "C:\Windows\Temp\fontdl"
     $Source      = "C:\Windows\Temp\fontdl\*"
